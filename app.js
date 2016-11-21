711b62910afc6499126493d481f47855d80df3dc
zdd-jgt <1821652461@qq.com>
2016-11-21 16:59:52 +0800

    shuye1.1

--- app.js
+++ app.js
@@ -1,8 +1,1 @@
 var app = angular.module("read",["ngRoute"]);
-app.config(["$routeProvider",function($routeProvider){
-    $routeProvider.when("/shuye1",{
-        templateUrl:"pages/book-page/shuye1/shuye.html"
-    }).when("/shuye1/shuye3",{
-        templateUrl:"pages/book-page/shuye3/shuye.html"
-    })
-}])
