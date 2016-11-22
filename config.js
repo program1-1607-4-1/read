b6c404f47b7103b968c51e6a74cf5ee507fb69b5
929781977@qq.com <929781977@qq.com>
2016-11-22 11:10:54 +0800

    添加配置

--- config.js
+++ config.js
@@ -41,7 +41,19 @@ app.config(["$routeProvider",function($routeProvider){
     }).when("/mulu",{
         templateUrl:"pages/zhangjie/mulu/mulu.html",
         controller:"muluCtrl"
-    }).when("/shuye1",{
+    }).when("/",{
+        templateUrl:"pages/bookshelf/shujia/shujia.html",
+        controller:"shujiaCtrl"
+    })..when("/findlist",{
+            templateUrl:"pages/bookshelf/findList/findList.html",
+            controller:"findlistCtrl"
+        })..when("/finddata",{
+            templateUrl:"pages/bookshelf/findData/findData.html",
+            controller:"finddataCtrl"
+        })..when("/filterl",{
+            templateUrl:"pages/bookshelf/filterl/filterl.html",
+            controller:"filterlCtrl"
+        }).when("/shuye1",{
         templateUrl:"pages/book-page/shuye1/shuye.html",
         controller:"shuye1Ctrl"
     }).when('/shuye1/shuye3',{
